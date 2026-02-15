7581dcfeb25579b33864d3f9b2c0934a33d58702
MHdi123 <mahdi1383akbari110@gmail.com>
2026-02-15 06:51:44 +0330

    Update app for Render port + fix manifest

--- app.py
+++ app.py
@@ -144,4 +144,7 @@ app = create_app()
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # فقط اگر جدول موجود نباشد ساخته می‌شود
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    
+    # استفاده از پورت اختصاصی Render یا 5000 لوکال
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
